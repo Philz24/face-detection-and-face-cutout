d9bd020771703c19991839813c7e935729b8fcbd
Philz24 <126084597+Philz24@users.noreply.github.com>
2026-08-27 12:46:04 -0400

    Correct Kankakee NOAA ISD station identifier

---
+++ tools/noaa_kankakee_run_v3.py
@@ -0,0 +1,7 @@
+import noaa_kankakee_build_v2 as build
+
+# Greater Kankakee Airport / KIKK: USAF 722127, WBAN 04880.
+build.STATION_ISD = '72212704880'
+
+if __name__ == '__main__':
+    build.main()
